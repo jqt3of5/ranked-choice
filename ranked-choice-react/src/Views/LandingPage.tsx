635af4f17bf8f4486cf393ce6865400adf329e3c
John Todd <jqt3of@gmail.com>
2022-04-17 18:43:08 -0600

    Use the create api and then forward

--- ranked-choice-react/src/Views/LandingPage.tsx
+++ ranked-choice-react/src/Views/LandingPage.tsx
@@ -1,30 +1,33 @@
 import './LandingPage.css'
 import '../Common/common.css'
-import { Link } from "react-router-dom";
 import {v4} from "uuid";
 import {useCookies} from "react-cookie";
 import {createElection} from "../Common/ElectionModel";
-import {useEffect, useState} from "react";
+import {useState} from "react";
+import { Navigate } from 'react-router-dom';
+
 
 export function LandingPage() {
 
     const [state, setState] = useState({electionId: ""})
 
-    useEffect()
     const [cookies, setCookie] = useCookies(['userId'])
     if (cookies.userId===undefined)
     {
         setCookie('userId', v4())
     }
+
     return <div className={"landing-page"}>
             <div className={"box landing-box"}>
-            <h2>Welcome to my ranked choice voting app!</h2>
+                <h2>Welcome to my ranked choice voting app!</h2>
+                {state.electionId != "" && <Navigate to={"/election/" + state.electionId}/>}
 
-            <Link onClick={e => {
-                createElection(cookies.userId).then(guid => {
+                <a onClick={e => {
+                    createElection(cookies.userId).then(guid => {
+                        setState(s => {return {...s, electionId: guid}})
+                    })
+                }}>Click here to start a new election</a>
 
-                })
-            }}>Click here to start a new election</Link>
         </div>
     </div>
 }
